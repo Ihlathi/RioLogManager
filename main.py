dbab6f2f6cf00921402c772e0aa6d9e8e802d556
Liam Smith <ihlathimc@gmail.com>
2026-02-15 14:34:27 -0500

    add main support, github actions builds

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from logapp!")
+import sys
+from PyQt6.QtWidgets import QApplication
+from log import MainWindow, QFont
 
+def main():
+    app = QApplication(sys.argv)
+    app.setFont(QFont("Open Sans", 10))
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
